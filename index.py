26234adc69f1946c565ada9e9e75971ef7743d5e
Alpha <alpha6662017@outlook.com>
2022-06-10 23:33:56 +0800

    更新了CU route

--- index.py
+++ index.py
@@ -1,20 +1,25 @@
 # pipreqs . --encoding=utf8 --force
 
-import model as model
+import module as module
 
 import flask
 
 App=flask.Flask(__name__)
 
+FireBase=module.database.DB()
+
 @App.route('/')
-def home():
-    return 'Hello'
+def Home():
+    return '這是臨時首頁'
 
 
 
+@App.route('/op/CUInfo',methods=['GET',"POST"])
+def NewTravelInfo():
+    if flask.request.method=='POST':
+        FireBase.FormDataGet('CU')
+    return flask.render_template('CUInfo.html')
 
-
-
-App.secret_key=model.tool.GetConfigData('Flask','SecretKey')
+App.secret_key=module.tool.GetConfigData('Flask','SecretKey')
 if __name__ == '__main__':
     App.run(host='127.0.0.1',port=8000,debug=True)
