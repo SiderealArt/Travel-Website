c1569644dfb548f3f1070dca8b767abc66ecfeb3
Alpha <alpha6662017@outlook.com>
2022-06-12 00:05:34 +0800

    database刪除功能完成

--- index.py
+++ index.py
@@ -10,19 +10,23 @@ App.config['JSON_AS_ASCII'] = False
 # Init Firebase
 FireBase=module.database.DB()
 
-@App.route('/')
+@App.route('/',methods=['GET','POST'])
 def Home():
     return '這是臨時首頁'
 
 @App.route('/api/TravelInfo')
 def TravelInfo():
-    return flask.jsonify(FireBase.STravelInfo())
+    return flask.jsonify(FireBase.Search('TravelInfo'))
 
-@App.route('/op/CUInfo',methods=['GET',"POST"])
-def CUInfo():
+@App.route('/op/Info',methods=['GET','POST'])
+def Info():
     if flask.request.method=='POST':
-        FireBase.FormDataGet('CU')
-    return flask.render_template('CUInfo.html')
+        if flask.request.values.get('ActionType')=='CU':
+            FireBase.HandleUpdateDatabase('CU','TravelInfo')
+        elif flask.request.values.get('ActionType')=='D':
+            FireBase.HandleUpdateDatabase('D','TravelInfo')
+    return flask.render_template('Info.html')
+
 
 App.config['secret_key']=module.tool.GetConfigData('Flask','SecretKey')
 if __name__ == '__main__':
