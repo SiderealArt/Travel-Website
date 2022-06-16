6291ec24d4334199616ffaf185980d03f75353e4
Alpha <alpha6662017@outlook.com>
2022-06-17 03:00:40 +0800

    修改按鈕樣式

--- static/scripts/Home.js
+++ static/scripts/Home.js
@@ -4,10 +4,10 @@ $.get('/api/TravelInfo',function (Data){
     while(Data[i]!=undefined){
         var RowData=Data[i];
         if(Flag[Number(RowData['Type'])]==0){
-            $('#carousel'+RowData['Type']+' .carousel-inner').append('<div class="carousel-item active"><img src="'+RowData['ImageUrl']+'" class="d-block w-100"><div class="carousel-caption d-none d-md-block">'+RowData['ShortContent']+'<a href="/Info/'+RowData['No']+'">查看更多</a></div>');
+            $('#carousel'+RowData['Type']+' .carousel-inner').append('<div class="carousel-item active"><img src="'+RowData['ImageUrl']+'" class="d-block w-100"><div class="carousel-caption d-none d-md-block">'+RowData['ShortContent']+'<a class="btn btn-success" role="button" href="/Info/'+RowData['No']+'">查看更多</a></div>');
         }else{
             $("#carousel"+RowData['Type']+" .carousel-indicators").append('<button type="button" data-bs-target="#carousel'+RowData['Type']+'" data-bs-slide-to="'+Flag[Number(RowData['Type'])]+'" aria-label="Slide '+Flag[Number(RowData['Type'])]+'"></button>');
-            $("#carousel"+RowData['Type']+" .carousel-inner").append('<div class="carousel-item"><img src="'+RowData['ImageUrl']+'" class="d-block w-100"><div class="carousel-caption d-none d-md-block">'+RowData['ShortContent']+'<a href="/Info/'+RowData['No']+'">查看更多</a></div>')
+            $("#carousel"+RowData['Type']+" .carousel-inner").append('<div class="carousel-item"><img src="'+RowData['ImageUrl']+'" class="d-block w-100"><div class="carousel-caption d-none d-md-block">'+RowData['ShortContent']+'<a class="btn btn-success" role="button" href="/Info/'+RowData['No']+'">查看更多</a></div>')
         }
         Flag[Number(RowData['Type'])]=Flag[Number(RowData['Type'])]+1;
         i++;
